f73bbd752cab9e97b0263e1e0dbd853f7a627d73
Julian <jgaray0817@gmail.com>
2017-03-31 17:57:25 -0400

    this sucks

--- kill me/link.cpp
+++ kill me/link.cpp
@@ -1,7 +1,6 @@
 #include "Link.h"
 #include <iostream>
 
-Node::Node(std::string name, Node* next) {
-  Node-> = name;
-  Node->next = next;
+Node::Node(std::string name, Node* next):name(name), next(next) {
+
 }
--- kill me/main.cpp
+++ kill me/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "link.h"
+#include "link.cpp"
 
 using namespace std;
 
 int main() {
+
   cout << "Enter a name. " << endl;
   cin >> name;
 
-  Node(Node.name, Node* next);
+
 }
